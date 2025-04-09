99863324dc1241485ed610ddf6a30bd121fb661d
Erika <156848482+Alexseka@users.noreply.github.com>
2025-04-09 00:31:58 -0400

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,43 @@
+const apiKey = 'a31a471c802f4ab9a5b34916250904';
+const baseURL = 'https://api.weatherapi.com/v1/current.json';
+
+function getWeatherData(cityName) 
+{
+    const url = `${baseURL}?key=${apiKey}&q=${cityName}&aqi=no`;
+    $.ajax({
+        url: url,
+        type: 'GET',
+        success: function(data) {
+            displayWeatherData(data);
+        },
+        error: function(xhr, status, error) {
+            console.log('Error status:', status);
+            console.log('Error response:', xhr.responseText);
+            $('#weatherResult').text('Failed retrieving data. Please try again.');
+        }
+    });
+}
+
+function displayWeatherData(data) 
+{
+    const weatherCondition = data.current.condition.text;
+    const iconUrl = `https:${data.current.condition.icon}`; 
+    const displayData = `
+      <div style="text-align:center;">
+      <img src="${iconUrl}" alt="${weatherCondition}" style="max-width: 100px; margin:0 1px 0 0;" />
+       </div>
+       <p>City: ${data.location.name}</p>
+       <p>Temperature: ${data.current.temp_c} °C</p>
+       <p>Weather: ${weatherCondition}</p>
+       <p>Humidity: ${data.current.humidity}%</p>
+    `;
+    $('#weatherResult').html(displayData).fadeIn();
+}
+
+$(document).ready(function() 
+  {
+    $('#getWeatherBtn').click(function() {
+        const cityName = $('#cityName').val();
+        getWeatherData(cityName);
+   });
+});
